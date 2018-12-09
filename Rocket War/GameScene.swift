362ee6024a7a1d5cd9b2adc7a0133242471491c1
Előd Mihály <mihalyelod@elods-macbook-pro.local>
2018-12-09 21:34:19 +0200

    Megcsinaltuk, hogy ki tudjuk loni az ellenfelet, es ha utkozunk az ellenfellel, akkor mindketto eltunik a kepernyorol. Es tettunk hozza robbanas effektet

--- Rocket War/GameScene.swift
+++ Rocket War/GameScene.swift
@@ -9,12 +9,18 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
     let player = SKSpriteNode(imageNamed: "playerShip")
     
     let bulletSound = SKAction.playSoundFileNamed("bulletSound.mp3", waitForCompletion: false)
     
+    struct PhysicsCategories{
+        static let None : UInt32 = 0
+        static let Player : UInt32 = 0b1 //1
+        static let Bullet : UInt32 = 0b10 //2
+        static let Enemy : UInt32 = 0b100 //4
+    }
     
     func random() -> CGFloat{
         return CGFloat(Float(arc4random()) / 0xFFFFFFFF)
@@ -23,6 +29,8 @@ class GameScene: SKScene {
     func random(min: CGFloat, max: CGFloat) -> CGFloat{
         return random() * (max - min) + min
     }
+    
+    
     
     // jatekter, ami igazodik az eszkoz mereteihez
     var gameArea: CGRect
@@ -44,6 +52,8 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        self.physicsWorld.contactDelegate = self
+        
         let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
         background.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
@@ -54,9 +64,77 @@ class GameScene: SKScene {
         player.setScale(1)
         player.position = CGPoint(x: self.size.width/2, y: self.size.height * 0.2)
         player.zPosition = 2
+        
+        // beallitja fizikai testnek a tankot, hogy majd esemenyeket tudjunk hozzaadni az objektumokhoz (pl. robbanas, utkozes)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody!.affectedByGravity = false
+        player.physicsBody!.categoryBitMask = PhysicsCategories.Player
+        player.physicsBody!.collisionBitMask = PhysicsCategories.None
+        player.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(player)
         
         startNewLevel()
+    }
+    
+    
+    func didBegin(_ contact:SKPhysicsContact){
+        
+        var body1 = SKPhysicsBody()
+        var body2 = SKPhysicsBody()
+        
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask{
+            body1 = contact.bodyA
+            body2 = contact.bodyB
+        }
+        
+        else{
+            body1 = contact.bodyB
+            body2 = contact.bodyA
+        }
+        
+        if body1.categoryBitMask == PhysicsCategories.Player && body2.categoryBitMask == PhysicsCategories.Enemy{
+            // ha a jatekos utkozik az ellenfellel
+            
+            if body1.node != nil{
+                spawnExplosion(spawnPosition: body1.node!.position)
+            }
+            
+            if body2.node != nil{
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+        
+        if body1.categoryBitMask == PhysicsCategories.Bullet && body2.categoryBitMask == PhysicsCategories.Enemy && (body2.node?.position.y)! < self.size.height{
+            // ha a golyo utkozik az ellenfellel
+            
+            if body2.node != nil{
+                spawnExplosion(spawnPosition: body2.node!.position)
+            }
+            
+            body1.node?.removeFromParent()
+            body2.node?.removeFromParent()
+        }
+        
+    }
+    
+    func spawnExplosion(spawnPosition: CGPoint){
+        
+        let explosion = SKSpriteNode(imageNamed: "explosion")
+        explosion.position = spawnPosition
+        explosion.zPosition = 3
+        explosion.setScale(0)
+        self.addChild(explosion)
+        
+        let scaleIn = SKAction.scale(to: 1, duration: 0.1)
+        let fadeOut = SKAction.fadeOut(withDuration: 0.1)
+        let delete = SKAction.removeFromParent()
+        
+        let explosionSequence = SKAction.sequence([scaleIn, fadeOut, delete])
+        
+        explosion.run(explosionSequence)
     }
     
     // var egy masodpercet minden ellenseg indulasanal
@@ -75,6 +153,11 @@ class GameScene: SKScene {
         bullet.setScale(1)
         bullet.position = player.position
         bullet.zPosition = 1
+        bullet.physicsBody = SKPhysicsBody(rectangleOf: bullet.size)
+        bullet.physicsBody!.affectedByGravity = false
+        bullet.physicsBody!.categoryBitMask = PhysicsCategories.Bullet
+        bullet.physicsBody!.collisionBitMask = PhysicsCategories.None
+        bullet.physicsBody!.contactTestBitMask = PhysicsCategories.Enemy
         self.addChild(bullet)
         
         let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
@@ -96,6 +179,11 @@ class GameScene: SKScene {
         enemy.setScale(1)
         enemy.position = startPoint
         enemy.zPosition = 2
+        enemy.physicsBody = SKPhysicsBody(rectangleOf: enemy.size)
+        enemy.physicsBody!.affectedByGravity = false
+        enemy.physicsBody!.categoryBitMask = PhysicsCategories.Enemy
+        enemy.physicsBody!.collisionBitMask = PhysicsCategories.None
+        enemy.physicsBody!.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.Bullet
         self.addChild(enemy)
         
         let moveEnemy = SKAction.move(to: endPoint, duration: 1.5)
